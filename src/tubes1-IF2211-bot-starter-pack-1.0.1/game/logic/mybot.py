eda77f80a4991ede7c41cd44f18e8d29849acace
Bodleh <tfprince079@gmail.com>
2024-03-04 19:34:04 +0700

    fix mybot.py

--- src/tubes1-IF2211-bot-starter-pack-1.0.1/game/logic/mybot.py
+++ src/tubes1-IF2211-bot-starter-pack-1.0.1/game/logic/mybot.py
@@ -8,7 +8,7 @@ import math
 
 
 def calculate_distance(a: Position, b: Position) -> int:
-    return (abs((a.x - b.x)**2 + (a.y - b.y)**2))
+    return ((a.x - b.x)**2 + (a.y - b.y)**2)
 
 
 def find_nearest_diamond(current: Position, diamonds: List[GameObject]) -> Optional[Position]:
@@ -23,16 +23,14 @@ def find_nearest_teleporter_pair(current: Position, teleporters: List[GameObject
     nearest_teleporter_pair = None
     min_distance = float('inf')
 
-    # Group teleporters
-    teleporter_pairs = {}
+    teleporter_pairs = {}  # Grouping Teleporters
     for teleporter in teleporters:
         pair_id = teleporter.properties.pair_id
         if pair_id not in teleporter_pairs:
             teleporter_pairs[pair_id] = []
         teleporter_pairs[pair_id].append(teleporter)
 
-    # Cari teleporter terdekat
-    for pair_id, pair in teleporter_pairs.items():
+    for pair_id, pair in teleporter_pairs.items():  # Cari teleporter terdekat
         if len(pair) == 2:
             teleporter, paired_teleporter = pair
             distance = calculate_distance(current, teleporter.position)
@@ -48,31 +46,23 @@ def get_direction_pribadi(current_x, current_y, dest_x, dest_y, avoid_teleporter
     delta_x = clamp(dest_x - current_x, -1, 1)
     delta_y = clamp(dest_y - current_y, -1, 1)
 
+    # Menghindar dari teleporter
     if (current_x + delta_x, current_y + delta_y) in avoid_teleporters and (dest_x, dest_y) not in avoid_teleporters:
-        if delta_x != 0:
-            if (current_x, current_y + 1) not in avoid_teleporters:
-                delta_y = 1
-            elif (current_x, current_y - 1) not in avoid_teleporters:
-                delta_y = -1
-            delta_x = 0
-        elif delta_y != 0:
-            if (current_x + 1, current_y) not in avoid_teleporters:
-                delta_x = 1
-            elif (current_x - 1, current_y) not in avoid_teleporters:
-                delta_x = -1
-            delta_y = 0
+        alternative_moves = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+        alternative_moves.remove((delta_x, delta_y))
+        for move in alternative_moves:
+            if (current_x + move[0], current_y + move[1]) not in avoid_teleporters:
+                return move
+        return (delta_x, delta_y)
     else:
         if delta_x != 0:
             delta_y = 0
-
     return (delta_x, delta_y)
 
 
 def is_on_path_or_close(diamond_position, bot_position, base_position, threshold):
-    # Check if the diamond is on the straight line path between the bot and the base
     on_path = (diamond_position.x == bot_position.x == base_position.x) or (
         diamond_position.y == bot_position.y == base_position.y)
-    # Check if the diamond is within the threshold distance from the path
     close_to_path = calculate_distance(diamond_position, bot_position) <= threshold or calculate_distance(
         diamond_position, base_position) <= threshold
     return on_path or close_to_path
@@ -90,7 +80,6 @@ class BertBots(BaseLogic):
         radius = board.height // 2
         time_left = props.milliseconds_left / 1000
         bot_position = board_bot.position
-        # print(board.height)
 
         diamond_game_objects = []
         teleport_game_objects = []
@@ -103,17 +92,21 @@ class BertBots(BaseLogic):
             elif obj.type == 'DiamondButtonGameObject':
                 diamond_button_game_objects.append(obj)
 
-        time_to_reach_base = round(math.sqrt(calculate_distance(
+        time_to_reach_base = math.ceil(math.sqrt(calculate_distance(
             board_bot.position, base)))
-        # print(board.game_objects)
-        # print(board_bot.properties.diamonds)
         nearest_diamond = find_nearest_diamond(
             bot_position, diamond_game_objects)
         nearest_teleporter_pair = find_nearest_teleporter_pair(
             bot_position, teleport_game_objects)
-        nearest_teleporter, paired_teleporter = nearest_teleporter_pair
+        if nearest_teleporter_pair:
+            teleporter1, teleporter2 = nearest_teleporter_pair
+            if calculate_distance(bot_position, teleporter1) <= calculate_distance(bot_position, teleporter2):
+                nearest_teleporter = teleporter1
+                paired_teleporter = teleporter2
+            else:
+                nearest_teleporter = teleporter2
+                paired_teleporter = teleporter1
 
-        # print(radius)
         if props.diamonds >= props.inventory_size - 1:
             distance_to_base = calculate_distance(
                 bot_position, base)
@@ -124,7 +117,7 @@ class BertBots(BaseLogic):
                 self.goal_position = nearest_teleporter
             else:
                 path_diamonds = [diamond for diamond in diamond_game_objects if is_on_path_or_close(
-                    diamond.position, bot_position, base, threshold=3) and diamond.properties.points + props.diamonds <= props.inventory_size]
+                    diamond.position, bot_position, base, threshold=4) and diamond.properties.points + props.diamonds <= props.inventory_size]
 
                 if path_diamonds:
                     nearest_path_diamond = find_nearest_diamond(
@@ -132,26 +125,24 @@ class BertBots(BaseLogic):
                     self.goal_position = nearest_path_diamond
                 else:
                     self.goal_position = base
+
         elif (props.diamonds >= 2 and time_left < time_to_reach_base+3):
-            self.goal_position = base
+            distance_to_base = calculate_distance(bot_position, base)
+            distance_to_base_via_teleport = calculate_distance(
+                paired_teleporter, base) + calculate_distance(bot_position, nearest_teleporter)
+            if distance_to_base_via_teleport < distance_to_base:
+                self.goal_position = nearest_teleporter
+            else:
+                self.goal_position = base
+
         elif not any(diamond for diamond in diamond_game_objects if calculate_distance(diamond.position, base) <= radius**2):
             nearest_diamond_button = find_nearest_diamond(
-                base, diamond_button_game_objects)
-            distance_to_diamond_button = calculate_distance(
-                bot_position, nearest_diamond_button)
-
-            # Find the nearest diamond to the nearest diamond button
-            nearest_diamond_to_button = find_nearest_diamond(
-                nearest_diamond_button, diamond_game_objects)
-            if nearest_diamond_to_button:
-                distance_diamond_to_button = calculate_distance(
-                    nearest_diamond_button, nearest_diamond_to_button)
-                if distance_to_diamond_button < distance_diamond_to_button:
-                    self.goal_position = nearest_diamond_button
-                else:
-                    self.goal_position = nearest_diamond_to_button
+                bot_position, diamond_button_game_objects)
+            if calculate_distance(bot_position, nearest_diamond) <= calculate_distance(bot_position, nearest_diamond_button):
+                self.goal_position = nearest_diamond
             else:
                 self.goal_position = nearest_diamond_button
+
         else:
             if nearest_diamond:
                 distance_to_diamond = calculate_distance(
@@ -160,19 +151,18 @@ class BertBots(BaseLogic):
                     self.goal_position = nearest_teleporter
                 else:
                     self.goal_position = nearest_diamond
+
             else:
                 self.goal_position = base
 
         if self.goal_position:
             if bot_position == nearest_teleporter and self.goal_position == nearest_teleporter:
-                # Move out of the teleporter
                 delta_x, delta_y = random.choice(
                     [(1, 0), (0, 1), (-1, 0), (0, -1)])
             else:
                 teleporter_positions = [
                     teleporter.position for teleporter in teleport_game_objects]
 
-                # Pass this list to get_direction
                 delta_x, delta_y = get_direction_pribadi(
                     bot_position.x,
                     bot_position.y,
